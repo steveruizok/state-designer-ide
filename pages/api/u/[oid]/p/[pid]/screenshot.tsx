547ac72f3ec2681000685cd35d9c625793ec1f95
Steve Ruiz <steveruizok@gmail.com>
2021-01-05 20:59:22 +0000

    Swaps capture tech
    Puppeteer wasn't working in the serverless function.

--- pages/api/u/[oid]/p/[pid]/screenshot.tsx
+++ pages/api/u/[oid]/p/[pid]/screenshot.tsx
@@ -1,24 +1,36 @@
-import { NextApiRequest, NextApiResponse } from "next"
+import * as playwright from "playwright-aws-lambda"
 
-import captureWebsite from "capture-website"
+import { NextApiRequest, NextApiResponse } from "next"
 
 export default async function sandbox(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   const {
-    query: { oid, pid, page = "chart" },
+    query: { oid, pid, view = "chart" },
   } = req
 
-  const url = `https://app.state-designer.com/u/${oid}/p/${pid}/${page.toString()}-clean`
+  const url = `https://app.state-designer.com/u/${oid}/p/${pid}/${view.toString()}-clean`
 
-  const buffer = await captureWebsite.buffer(url, {
-    width: 1200,
-    height: 630,
-    delay: 2,
-    scaleFactor: 1,
+  const browser = await playwright.launchChromium()
+  const page = await browser.newPage({
+    viewport: {
+      width: 1200,
+      height: 630,
+    },
+  })
+  await page.goto(url, {
+    timeout: 15 * 1000,
   })
 
-  res.status(200).end(buffer)
+  await page.waitForTimeout(1500)
+
+  const data = await page.screenshot({
+    type: "png",
+  })
+  await browser.close()
+  res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate")
+  res.setHeader("Content-Type", "image/png")
+  res.status(200).end(data)
   return {}
 }
